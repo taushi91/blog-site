2988efe050ae0a0935dd0929eac08aa5771346aa
Md. Zahid Hasan <zahid.cse07@gmail.com>
2018-03-08 01:28:38 +0600

    contact page update

--- contact.php
+++ contact.php
@@ -22,8 +22,8 @@
                 <div class="col-12">
                     <nav aria-label="breadcrumb">
                         <ol class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="#"><i class="fa fa-home" aria-hidden="true"></i> Home</a></li>
-                            <li class="breadcrumb-item active" aria-current="page">Contact Us</li>
+                            <li class="breadcrumb-item"><a href="index.php"><i class="fa fa-home" aria-hidden="true"></i> হোম</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">যোগাযোগ করুন</li>
                         </ol>
                     </nav>
                 </div>
@@ -38,7 +38,9 @@
             <div class="row">
                 <div class="col-12">
                     <div class="google-map-area">
-                        <div id="googleMap"></div>
+                        <div id="">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d233667.82239252984!2d90.2792377959246!3d23.780887456160208!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b8b087026b81%3A0x8fa563bbdd5904c2!2sDhaka!5e0!3m2!1sen!2sbd!4v1520369004308" width="960" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                        </div>
                     </div>
                 </div>
             </div>
